c43c5f4c4e924e3aab4ad60c6a2187eef7fdde46
Pooja Das <poojadas@gmail.com>
2025-03-21 10:51:35 +0530

    Initial CI/CD setup

---
+++ .github/workflows/terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "CI-CD-WebApp"
+  }
+}
